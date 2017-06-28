4922594c80a7d5422cc4d5ebc069cacba167dadc
Shem Sedrick <ssedrick1@gmail.com>
2017-06-27 22:20:18 -0600

    Set up redux devtools to only be included based on environment

--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -4,12 +4,18 @@ import promise from 'redux-promise'
 import { routerMiddleware } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 import rootReducer from '../reducers';
+import environment from '../constants/environment.js';
 import * as types from '../constants/ActionTypes'
 const debugware = [];
 
-const composeEnhancers = compose;
 
 export default function configureStore(initialState) {
+	let composeEnhancers;
+	if (environment == 'production') {
+		composeEnhancers = compose;
+	} else {
+		composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+	}
 
    const store = createStore(
       rootReducer,
