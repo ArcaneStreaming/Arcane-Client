b658d0665710d66874a4a67c07b8035ae4a34943
Shem Sedrick <ssedrick1@gmail.com>
2017-06-27 22:22:34 -0600

    Getting ready to add fab-menu

--- src/components/FloatingControls.jsx
+++ src/components/FloatingControls.jsx
@@ -4,59 +4,61 @@ import theme from '../constants/material-ui-theme'
 // import * as ActionTypes from '../constants/ActionTypes'
 
 const style = {
-  fab: {
-   position: 'absolute',
-   right: '0',
-   bottom: '0',
-   marginBottom: '5vh',
-   marginRight: '5vh',
-  zIndex:1
- },
-
-   progress: {
-      position: 'absolute',
-      right: '0',
-      bottom: '0',
-      MsTransform: 'rotate(-90deg)',
-      WebkitTransform: 'rotate(-90deg)',
-      transform: 'rotate(-90deg)'
-   }
+	fab: {
+		position: 'absolute',
+		right: '0',
+		bottom: '0',
+		marginBottom: '5vh',
+		marginRight: '5vh',
+		zIndex:1
+	},
+	progress: {
+		position: 'absolute',
+		right: '0',
+		bottom: '0',
+		MsTransform: 'rotate(-90deg)',
+		WebkitTransform: 'rotate(-90deg)',
+		transform: 'rotate(-90deg)'
+	}
 }
 
+const controls = [
+	{ "id": 1, "name": "skip_next" },
+	{ "id": 2, "name": "skip_previous" },
+	{ "id": 3, "name": "shuffle" },
+	{ "id": 4, "name": "repeat" },
+	{ "id": 5, "name": "favorite_border" },
+]
+
 export default class FloatingControls extends Component {
-   constructor(props) {
-      super(props);
-   }
+	constructor(props) {
+		super(props);
+	}
 
-  //  renderAddQueueButton() {
-  //    if (this.state.selected) {
-  //      return(
-  //        <FloatingActionButton onClick={this.props.pushToQueue} style={style.left} >
-  //          <FontIcon className="material-icons">queue</FontIcon>
-  //        </FloatingActionButton>
-  //      );
-  //    }
-  //  }
-   render() {
-      return (
+	renderFabMenu() {
+		let fabs = [];
 
-        <div>
-          <FloatingActionButton
-            onClick={this.props.onPlay}
-            style={style.fab}
-          >
-            <FontIcon className="material-icons">{this.props.isPlaying ? "pause" : "play_arrow"}</FontIcon>
-            <CircularProgress
-              color={theme.palette.alternateTextColor}
-              mode="determinate"
-              size={57}
-              style={style.progress}
-              thickness={3}
-              value={this.props.percent * 100}
-            />
-          </FloatingActionButton>
-          {/* {this.renderAddQueueButton()} */}
-        </div>
-      );
-   }
+		return fabs;
+	}
+
+	render() {
+		return (
+			<div>
+				<FloatingActionButton
+					onClick={this.props.onPlay}
+					style={style.fab}
+					>
+					<FontIcon className="material-icons">{this.props.isPlaying ? "pause" : "play_arrow"}</FontIcon>
+					<CircularProgress
+						color={theme.palette.alternateTextColor}
+						mode="determinate"
+						size={57}
+						style={style.progress}
+						thickness={3}
+						value={this.props.percent * 100}
+						/>
+				</FloatingActionButton>
+			</div>
+		);
+	}
 }
