424f088aa63bf121bc706b9d2228ef47c8cf7621
Shem Sedrick <ssedrick1@gmail.com>
2017-06-27 22:20:53 -0600

    Fixed tab/spacing that was bothering me

--- src/reducers/artists.jsx
+++ src/reducers/artists.jsx
@@ -1,25 +1,26 @@
 import {GET_ARTISTS, GET_GENRE_ARTISTS, GET_NEXT_ARTISTS} from '../constants/ActionTypes';
 
 const initialState = {
-   allArtists: [],
-   genreArtists: []
+	allArtists: [],
+	genreArtists: []
 };
+
 export default function artists(state = initialState, action) {
-  let allArtists = []
-  switch (action.type){
-    case GET_ARTISTS:
-      allArtists = action.artists;
-      return { ...state, allArtists};
-    case GET_NEXT_ARTISTS:
-      let artistsArray = state.allArtists.results;
-      artistsArray = artistsArray.concat(action.artists.results);
-      allArtists = action.artists;
-      allArtists.results = artistsArray;
-      return {...state, allArtists: allArtists};
-    case GET_GENRE_ARTISTS:
-      const genreArtists = action.artists;
-      return {...state, genreArtists};
-    default:
-      return state;
-   }
+	let allArtists = []
+	switch (action.type){
+		case GET_ARTISTS:
+			allArtists = action.artists;
+			return { ...state, allArtists};
+		case GET_NEXT_ARTISTS:
+			let artistsArray = state.allArtists.results;
+			artistsArray = artistsArray.concat(action.artists.results);
+			allArtists = action.artists;
+			allArtists.results = artistsArray;
+			return {...state, allArtists: allArtists};
+		case GET_GENRE_ARTISTS:
+			const genreArtists = action.artists;
+			return {...state, genreArtists};
+		default:
+			return state;
+	}
 }
